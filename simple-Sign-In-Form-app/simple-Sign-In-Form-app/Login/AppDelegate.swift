254f12506b74b88c5c006bdab9a7c6cb89584623
Georgiy <lev.davidovich1@yandex.ru>
2023-04-08 18:17:59 +1000

    add project

---
+++ simple-Sign-In-Form-app/simple-Sign-In-Form-app/Login/AppDelegate.swift
@@ -0,0 +1,79 @@
+//
+//  AppDelegate.swift
+//  simple-Sign-In-Form-app
+//
+//  Created by Georgiy on 08.10.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    var hasOnboarded = false
+    let loginViewController = LoginViewController()
+    let onboardingContainerViewController = OnboardingContainerViewController()
+    let dummyViewController = DummyViewController()
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .systemBackground
+        
+        loginViewController.delegate = self
+        onboardingContainerViewController.delegate = self
+        dummyViewController.logoutDelegate = self
+        
+        window?.rootViewController = onboardingContainerViewController
+        //window?.rootViewController = loginViewController
+        return true
+    }
+}
+
+extension AppDelegate {
+    
+    func setRootViewController(_ vc: UIViewController, animated: Bool = true) {
+        guard animated, let window = self.window else {
+            self.window?.rootViewController = vc
+            self.window?.makeKeyAndVisible()
+            return
+        }
+        
+        window.rootViewController = vc
+        window.makeKeyAndVisible()
+        UIView.transition(with: window,
+                          duration: 0.5,
+                          options: .transitionCrossDissolve,
+                          animations: nil,
+                          completion: nil)
+    }
+}
+
+extension AppDelegate: LoginViewControllerDelegate {
+    
+    func didLogin() {
+        //LocalState.hasOnboarded
+        if hasOnboarded {
+            setRootViewController(dummyViewController)
+        } else {
+            setRootViewController(onboardingContainerViewController)
+        }
+    }
+}
+
+extension AppDelegate: OnboardingContainerViewControllerDelegate {
+    
+    func didFinishOnboarding() {
+        //LocalState.hasOnboarded = true
+        setRootViewController(dummyViewController)
+    }
+}
+
+extension AppDelegate: LogoutDelegate {
+    
+    func didLogout() {
+        setRootViewController(loginViewController)
+    }
+}
